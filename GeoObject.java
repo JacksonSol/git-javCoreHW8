79e6e6d3ca63266d47e0173efcfc9d2561642cdd
JacksonSol <94706006+JacksonSol@users.noreply.github.com>
2022-10-20 21:05:23 +0300

    Add files via upload

---
+++ GeoObject.java
@@ -0,0 +1,17 @@
+package javaCoreHW8;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class GeoObject {
+    @JsonProperty("locality")
+    Locality LocalityObject;
+
+    public Locality getLocalityObject() {
+        return LocalityObject;
+    }
+
+    public void setLocalityObject(Locality localityObject) {
+        LocalityObject = localityObject;
+    }
+
+}
---
+++ Locality.java
@@ -0,0 +1,13 @@
+package javaCoreHW8;
+
+public class Locality {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
